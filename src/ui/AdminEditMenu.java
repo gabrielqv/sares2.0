607c4cac3d8ea4886655e07843df536218d8b8e9
gabrielqv <gabrielquinche@hotmail.com>
2019-12-10 16:22:32 -0500

    cambio de vinculos

--- src/ui/AdminEditMenu.java
+++
@@ -1,9 +0,0 @@
-package com.kagada.sares.ui;
-
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
-
-@Route("AdminEdit")
-public class AdminEditMenu extends VerticalLayout {
-
-}
